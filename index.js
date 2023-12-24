85e9ced2f89ed8b9fdbd944b1f44d29e0d983227
navoren <namantomar1453@gmail.com>
2023-12-24 23:10:01 +0530

    Using Express module

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
-const http = require("http"); //this module helps in creating a server
-const fs = require("fs"); //this module helps in reading and writing files
-const url = require("url"); //this module helps to parse the url
+const express = require("express"); // express module is imported
+const app = express(); //handler function is created using express framework
+
+app.get('/', (req, res) => {
+    return res.send('Welcome to the Home Page');
+});
+
+app.get('/about', (req, res) => {
+    return res.send('Welcome to the About Page');
+});
+
+app.get('/name', (req, res) => {
+   return res.send(`Hello ${req.query.name} you are ${req.query.age} years old`);
+});
+
 const { Console } = require("console");
+   
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
 
-
-//handler function
-//creates a server object
-const myServer = http.createServer((req, res) => { //it takes two parameters request and response 
-    if(req.url ==='/favicon.ico'){ //this is to prevent the favicon request from being logged in the log file
-        res.end();
-        return;
-    }
-
-    const log = `${Date.now()} : ${req.url} New Req Recieved\n`;
-    const myUrl = url.parse(req.url, true); //parse helps in parsing the url and true is to parse the query string
-
-    console.log(myUrl);
-
-    fs.appendFile('log.txt', log, (err, data) => {
-        switch (myUrl.pathname) { //pathname is the path of the url //switch case helps in navigating the url and query parameters
-            case '/':
-                res.end('Welcome to the Home Page');
-                break;
-            case '/about':
-                res.end('Welcome to the About Page');
-                break;
-            case '/contact':
-                res.end('Welcome to the Contact Page');
-                break;
-            default:
-                res.end('404 Page Not Found');
-                break;
-        }
-    });
-    });
-
-myServer.listen(3000, () => console.log('server is listening on port 3000...')); //the server object listens on port 3000 this is a callback function
-
-//if we create handler function on our own it creates a mess of the code, so we use express framework to create handler function
---
+++ notes/Express.md
@@ -0,0 +1,17 @@
+## Using Express.js instead of the traditional way of creating a server with the http module
+
+When developing a web application, we often need to create a server to handle incoming requests and serve responses. Traditionally, this was done using the built-in `http` module in Node.js. However, as applications became more complex, it became clear that a more robust and flexible framework was needed.
+
+This is where Express.js comes in. Express.js is a fast, unopinionated, and minimalist web framework for Node.js. It provides a simple and intuitive way to handle routing, middleware, and other common web development tasks.
+
+Here are some reasons why we might choose to use Express.js instead of the traditional way of creating a server with the `http` module:
+
+1. **Simplicity**: Express.js provides a higher level of abstraction, making it easier to define routes, handle requests, and manage middleware. This can greatly simplify the development process and reduce boilerplate code.
+
+2. **Routing**: Express.js has a powerful routing system that allows us to define routes for different HTTP methods and URLs. This makes it easy to handle different types of requests and implement RESTful APIs.
+
+3. **Middleware**: Express.js has a middleware architecture that allows us to add functionality to our application in a modular and reusable way. We can easily add middleware for tasks such as logging, authentication, error handling, and more.
+
+4. **Community and Ecosystem**: Express.js has a large and active community, which means there are plenty of resources, tutorials, and plugins available. This makes it easier to find solutions to common problems and extend the functionality of our application.
+
+Overall, using Express.js instead of the traditional way of creating a server with the `http` module can greatly simplify the development process, improve code organization, and provide a more robust and flexible foundation for our web application.
